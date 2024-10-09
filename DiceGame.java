dd3cfdcc2e4b92ddcb2e9f0c214c6dbc87378d8b
Steve Banh <steve.banh02@gmail.com>
2024-10-09 14:29:55 -0400

    final dice game

--- DiceGame.java
+++ DiceGame.java
@@ -1,56 +1,50 @@
 import java.util.Scanner;
 public class DiceGame{
 
-    int num1, num2, num3;
-
-    public DiceGame(int tempNum1, int tempNum2, int tempNum3){
-        num1 = tempNum1;
-        num2 = tempNum2;
-        num3 = tempNum3;
-    }
-    
-    // public void randomNumbers(){
-    //     int randomNum1 = (int)(Math.random()*6 + 1);
-    //     int randomNum2 = (int)(Math.random()*6 + 1);
-
-    //     System.out.println(randomNum1);
-    //     System.out.println(randomNum2);
-    // }
-
-    public void guessNumRight(int number){
-
-        int randomNum = (int)(Math.random()*6 + 1);
+    public static boolean checkUserGuessNum(int guessNum1, int guessNum2, int die1, int die2, int sumDice){
         
-        if (number == randomNum){
-            System.out.println("congrads you got the right number");
-        }else {
-            System.out.println("better luck next time");
+        if (guessNum1 == die1 && guessNum2 == die2){
+            
+            System.out.println("Congradulations! You won! You guessed both numbers correctly.");
+            return true;
+
+        }else if (die1 == die2) {
+
+            System.out.println("Congradulations! you won! You rolled both dice the same number.");
+            return true;
+
+        }else if (sumDice == 6){
+
+            System.out.println("Congradulations! You won! Both dice sums up to 6");
+            return true;
+
+        }else{
+            
+            System.out.println("Better luck next time!");
+            return false;
         }
-
-        System.out.println("the random number is " + randomNum);
-
     }
 
     public static void main (String[]args){
         
         Scanner input = new Scanner (System.in);
 
-        DiceGame myNum = new DiceGame(0,0,0);
+        int die1 = (int)(Math.random()*6 + 1);
+        int die2 = (int)(Math.random()*6 + 1);
+        int sumDice = die1 + die2;
 
-        System.out.println("Enter your first number guess: ");
-        int numOne = input.nextInt();
-
-        myNum.guessNumRight(numOne);
+        System.out.println("Enter your first number: ");
+        int guessNum1 = input.nextInt();
         
-        System.out.println("enter your second number guess: ");
-        int numTwo = input.nextInt();
+        System.out.println("Enter your second number: ");
+        int guessNum2 = input.nextInt();
 
-        myNum.guessNumRight(numTwo);
-        //System.out.println(myNum);
+        if(checkUserGuessNum(guessNum1, guessNum2, die1, die2, sumDice)){
 
-        
-        
+            System.out.println("The first die number is " + die1);
+            System.out.println("The second die number is " + die2);
+            System.out.println("The sum of the dice is " + sumDice);
 
-
+        }
     }
 }
